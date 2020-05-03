3d5af64d5c60d7319531cfad54946e4be000517c
Isadora Queiroz <isadora.q@gmail.com>
2020-05-03 19:48:25 -0300

    iniciado algoritmo de validacao

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 console.log("Javascript carregado");
 
 function validaCPF(cpf) {
-  return false;
+  console.log(cpf.length);
+  if(cpf.length != 11){
+  	return false;
+  } else {
+  	var numeros = cpf.substring(0, 9);
+  	var digitos = cpf.substring(9);
+
+  	console.log("numeros do cpf " + numeros);
+  	console.log("digitos do cpf " + digitos);
+
+  	var soma = 0;
+  	for (var i = 10; i > 1; i--){
+  		soma += numeros.charAt(10 - i) * i;
+  	}
+  	console.log("resultado " + soma);
+  	return true;
+  }
 }
 
 function validacao() {
