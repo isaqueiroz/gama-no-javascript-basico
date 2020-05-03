e73929355c6eb242c8f0cac6c7b7799814cb2244
Isadora Queiroz <isadora.q@gmail.com>
2020-05-03 20:09:26 -0300

    validacao do segundo digito

--- script.js
+++ script.js
@@ -21,10 +21,20 @@ function validaCPF(cpf) {
   	// validacao do primeiro digito
   	if (resultado != digitos.charAt(0)){
   		return false;
-  	} else {
+  	} 
 
-  	  console.log(digitos.charAt(0) + ' é a primeira posicao da variavel soma');
+  	soma = 0;
+  	numeros = cpf.substring(0, 10);
+  	for (var k = 11; k > 1; k--) {
+  		soma += numeros.charAt(11 - k) * k;
   	}
+
+  	resultado = soma % 11 < 2 ? 0 : 11 - (soma % 11);
+  	// validacao do segundo digito
+  	if (resultado != digitos.charAt(1)) {
+  		return false;
+  	}
+
   	return true;
   }
 }
@@ -37,7 +47,9 @@ function validacao() {
   var resultadoValidacao = validaCPF(cpf);
   if (resultadoValidacao) {
   	document.getElementById('success').style.display = 'block';
+  	document.getElementById('error').style.display = 'none';
   } else {
+  	document.getElementById('success').style.display = 'none';
   	document.getElementById('error').style.display = 'block';
   }
 }
