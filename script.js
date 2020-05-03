985fd92e353abd95f0576140469bca236128cc90
Isadora Queiroz <isadora.q@gmail.com>
2020-05-03 19:35:01 -0300

    Feito script do botao de validacao e mensagem com resultado

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 console.log("Javascript carregado");
+
+function validaCPF(cpf) {
+  return false;
+}
+
+function validacao() {
+  console.log('Iniciando validacao CPF');
+
+  var cpf = document.getElementById('cpf_digitado').value;
+
+  var resultadoValidacao = validaCPF(cpf);
+  if (resultadoValidacao) {
+  	document.getElementById('success').style.display = 'block';
+  } else {
+  	document.getElementById('error').style.display = 'block';
+  }
+}
